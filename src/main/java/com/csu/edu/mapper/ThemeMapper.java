a9d0680ae93ed3d423e2a9304cef05a64c819791
Arseny <56878785+jawwr@users.noreply.github.com>
2024-11-03 21:37:55 +0500

    OKR-32 Add creating themes (#13)

--- src/main/java/com/csu/edu/mapper/ThemeMapper.java
+++ src/main/java/com/csu/edu/mapper/ThemeMapper.java
@@ -1,8 +1,11 @@
 package com.csu.edu.mapper;
 
 import com.csu.edu.config.MapperConfig;
+import com.csu.edu.dto.themes.CreateThemeDto;
 import com.csu.edu.dto.themes.ThemeDto;
 import com.csu.edu.dto.themes.ThemeInfo;
+import com.csu.edu.model.Category;
+import com.csu.edu.model.Image;
 import com.csu.edu.model.Theme;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -15,4 +18,11 @@ public interface ThemeMapper {
 
     @Mapping(target = "imageLink", source = "image.link")
     ThemeDto toThemeDto(Theme theme);
+
+    @Mapping(target = "name", source = "dto.name")
+    @Mapping(target = "description", source = "dto.description")
+    @Mapping(target = "image", source = "image")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "categoryId", ignore = true)
+    Theme fromCreateThemeDto(CreateThemeDto dto, Category category, Image image);
 }
--- src/main/java/com/csu/edu/service/ThemeService.java
+++ src/main/java/com/csu/edu/service/ThemeService.java
@@ -1,9 +1,12 @@
 package com.csu.edu.service;
 
+import com.csu.edu.dto.themes.CreateThemeDto;
 import com.csu.edu.dto.themes.ThemeDto;
 import com.csu.edu.dto.themes.ThemeInfo;
 import com.csu.edu.exception.DataNotFoundException;
 import com.csu.edu.mapper.ThemeMapper;
+import com.csu.edu.model.Category;
+import com.csu.edu.model.Image;
 import com.csu.edu.model.Theme;
 import com.csu.edu.repository.ThemeRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +23,8 @@ public class ThemeService {
 
     private final ThemeRepository repository;
     private final ThemeMapper mapper;
+    private final CategoryService categoryService;
+    private final ImageService imageService;
 
     @Transactional(readOnly = true)
     public List<ThemeInfo> getAllThemes(Long categoryId) {
@@ -34,5 +39,13 @@ public class ThemeService {
         return repository.findByIdWithImage(id)
                 .map(mapper::toThemeDto)
                 .orElseThrow(() -> new DataNotFoundException(THEME_DOES_NOT_EXIST_MESSAGE.formatted(id)));
+    }
+
+    @Transactional
+    public void createTheme(CreateThemeDto createDto) {
+        Category category = categoryService.findByIdOrElseThrow(createDto.categoryId());
+        Image image = imageService.createImage(createDto.imageFile());
+        Theme theme = mapper.fromCreateThemeDto(createDto, category, image);
+        repository.save(theme);
     }
 }
